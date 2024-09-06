3eb753b6fdddea5fe80933105b075e1c913a94e3
laura <laura@PESCIOLINO>
2024-09-06 17:14:15 +0200

    Add project files.

---
+++ Tic-tac-toe/Game.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace Tic_tac_toe
+{
+    public class Game
+    {
+        enum Mark
+        {
+            X,
+            O,
+            Empty
+        }
+
+        Dictionary<string, string> playerMoveDict = new Dictionary<string, string>();
+        List<string> winningCombinations = new List<string>();
+
+        public Game()
+        {
+            playerMoveDict.Add("Player 1", "X");
+            playerMoveDict.Add("Player 2", "O");
+
+            winningCombinations.Add("//1 2 3");
+            winningCombinations.Add("//1 4 7");
+            winningCombinations.Add("//1 5 9");
+            winningCombinations.Add("//2 5 8");
+            winningCombinations.Add("//3 6 9");
+            winningCombinations.Add("//3 5 7");
+            winningCombinations.Add("//4 5 6");
+            winningCombinations.Add("//7 8 9");
+        }
+
+        public void ClearGameField(TextBox[] fieldslist)
+        {
+            foreach (TextBox field in fieldslist)
+                field.Text = "";
+        }
+
+        public string MakeAMove(string player)
+        {
+            return playerMoveDict[player];
+        }
+
+        public SolidColorBrush SetMarkColor(string player)
+        {
+            if (player == "Player 1")
+                return new SolidColorBrush(Colors.Blue);
+            else
+                return new SolidColorBrush(Colors.Red);
+        }
+
+        public string NextPlayer(string currentPlayer)
+        {
+            if (currentPlayer == "Player 1")
+                return "Player 2";
+            else
+                return "Player 1";
+        }
+
+        public bool CheckWin(string fields)
+        {
+            // Winning combinations:
+            //1 2 3;    1 4 7;  1 5 9;  2 5 8;
+            //3 6 9;    3 5 7;  4 5 6;  7 8 9.
+            if (fields.Contains('1') && fields.Contains('2') && fields.Contains('3') ||
+                fields.Contains('1') && fields.Contains('4') && fields.Contains('7') ||
+                fields.Contains('1') && fields.Contains('5') && fields.Contains('9') ||
+                fields.Contains('2') && fields.Contains('5') && fields.Contains('8') ||
+                fields.Contains('3') && fields.Contains('6') && fields.Contains('9') ||
+                fields.Contains('3') && fields.Contains('5') && fields.Contains('7') ||
+                fields.Contains('4') && fields.Contains('5') && fields.Contains('6') ||
+                fields.Contains('7') && fields.Contains('8') && fields.Contains('9'))
+                return true;
+             else
+                return false;
+        }
+    }
+}
---
+++ Tic-tac-toe/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tic_tac_toe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBox[] fieldsList = new TextBox[9];
+        private string _fieldsWithX;
+        private string _fieldsWithO;
+        public MainWindow()
+        {
+            InitializeComponent();
+            fieldsList[0] = Field1;
+            fieldsList[1] = Field2;
+            fieldsList[2] = Field3;
+            fieldsList[3] = Field4;
+            fieldsList[4] = Field5;
+            fieldsList[5] = Field6;
+            fieldsList[6] = Field7;
+            fieldsList[7] = Field8;
+            fieldsList[8] = Field9;
+        }
+
+        
+        private void BtnStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            game.ClearGameField(fieldsList);
+            TxtBox_Player.Text = "Player 1";
+            TxtBox_Player.Visibility = Visibility.Visible;
+            TxtBox_MakeyourMove.Visibility = Visibility.Visible;
+            foreach( TextBox field in fieldsList)
+                field.Visibility = Visibility.Visible;
+
+            _fieldsWithX = "";
+            _fieldsWithO = "";
+        }
+
+        private void PlaceAMark_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                TextBox field = (TextBox)sender;
+                if (field.Text == "")
+                {
+                    bool isWinX = false;
+                    bool isWinO = false;
+                    char fieldNumber = field.Name[field.Name.Length - 1];
+
+                    Game game = new Game();
+
+                    field.Text = game.MakeAMove(TxtBox_Player.Text);
+                    field.Foreground = game.SetMarkColor(TxtBox_Player.Text);
+
+                    if (TxtBox_Player.Text == "Player 1")
+                    {
+                        _fieldsWithX += fieldNumber + " ";
+                        if (_fieldsWithX.Length > 4)
+                            isWinX = game.CheckWin(_fieldsWithX);
+                    }
+                    else
+                    {
+                        _fieldsWithO += fieldNumber + " ";
+                        if (_fieldsWithO.Length > 4)
+                            isWinO = game.CheckWin(_fieldsWithO);
+                    }
+                    if (isWinX)
+                        MessageBox.Show("Player 1 wins!");
+                    if (isWinO)
+                        MessageBox.Show("Player 2 wins!");
+
+                    if (!isWinX && !isWinO)
+                    {
+                        if(_fieldsWithX.Length + _fieldsWithO.Length == 18)
+                            MessageBox.Show("It's a tie!");
+                        else
+                            TxtBox_Player.Text = game.NextPlayer(TxtBox_Player.Text);
+                    }
+                }
+            }
+        }
+    }
+}
